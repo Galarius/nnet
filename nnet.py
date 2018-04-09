1f37909eb068c27af04fa6ef38e39cb781bed7b7
Ilya Shoshin <galarius.is@gmail.com>
2018-04-09 12:13:33 +0300

    Bias weights corrected.

--- nnet.py
+++ nnet.py
@@ -59,26 +59,26 @@ class NeuralNetwork(object):
         grads.append(self.ys[-1].T.dot(delta))
         b_error = error
         b_delta = delta
-        bias_grads.append(b_delta)
+        b_grad = np.zeros((self.w_biases[-1].shape[0],b_delta.shape[0])).dot(b_delta)
+        bias_grads.append(b_grad)
         # ошибки для скрытых слоёв
         if self.n_hlayers > 1:
-            for w in self.w_layers:
-                print w
             for i in range(self.n_hlayers - 1, 0, -1):
-                print i
                 error = delta.dot(self.w_layers[i+1].T)
                 delta = error * self.sigmoid_prime(self.ys[i])
                 grads.append(self.ys[i-1].T.dot(delta))
                 b_error = b_delta.dot(self.w_biases[i+1].T)
                 b_delta = b_error * self.sigmoid_prime(self.ys[i])
-                bias_grads.append(b_delta)
+                b_grad = np.zeros((self.w_biases[i].shape[0],b_delta.shape[0])).dot(b_delta)
+                bias_grads.append(b_grad)
         # ошибка для входного слоя
         error = delta.dot(self.w_layers[1].T)
         delta = error * self.sigmoid_prime(self.ys[0])
         grads.append(x.T.dot(delta))
         b_error = delta.dot(self.w_biases[1].T)
         b_delta = b_error * self.sigmoid_prime(self.ys[0])
-        bias_grads.append(b_delta)
+        b_grad = np.zeros((self.w_biases[0].shape[0],b_delta.shape[0])).dot(b_delta)
+        bias_grads.append(b_grad)
         # корректировка весов
         s = len(grads)
         for i in range(0, s):
@@ -113,6 +113,8 @@ class NeuralNetwork(object):
         "Сохранение весов в файлы"
         for i, w in enumerate(self.w_layers):
             np.savetxt("{}_{}.w.txt".format(prefix, i), w, fmt="%s")
+        for i, w in enumerate(self.w_biases):
+            np.savetxt("{}_bias_{}.w.txt".format(prefix, i), w, fmt="%s")
     
     def load_weights(self, prefix):
         "Загрузка весов из файлов"
@@ -123,6 +125,8 @@ class NeuralNetwork(object):
         shape = self.w_layers[-1].shape
         self.w_layers[-1] = np.loadtxt("{}_{}.w.txt".format(prefix, last), dtype=float)
         self.w_layers[-1] = self.w_layers[-1].reshape(shape[0], self.n_out)
+        for i in range(0, len(self.w_biases)):
+            self.w_biases[i] = np.loadtxt("{}_bias_{}.w.txt".format(prefix, i), dtype=float)
 
     def __str__(self):
         return "\nInputs:  {0}\nOutputs: {1}\nHidden:  {2}\nNeurons in Hidden: {3}\nUse Bias: {4}\n".format(self.n_in, self.n_out, self.n_hlayers, self.n_hl, 'Yes' if self.use_bias else 'No')
